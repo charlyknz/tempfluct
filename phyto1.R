0c1f56f25aa13d63bdee83885aa36cdd24e504b0
charlyknz <45939434+charlyknz@users.noreply.github.com>
2020-07-28 12:14:43 +0200

    Add files via upload
    new phytoplankton analysis, calculating BV and ARF

--- phyto1.R
+++ phyto1.R
@@ -11,17 +11,18 @@ library(vegan)
 
 
 # import dataset
-data <- read_delim("~/Desktop/MA/MA_Rcode/project_data/phytoplankton_counts_may2020.csv", 
+data <- read_delim("~/Desktop/MA/MA_Rcode/project_data/phyto_new.csv", 
                    ";", escape_double = FALSE, col_types = cols(date = col_character()), 
                    locale = locale(decimal_mark = ","), 
-                   trim_ws = TRUE) 
+                   trim_ws = TRUE) %>%
+  drop_na(MC)
 View(data)
 str(data)
 
 
 #change variable format to numeric 
 data$cells_ml <- as.numeric(gsub(",", ".", data$cells_ml))
-data1 <- filter(data, !comment %in% 'recount')
+#data1 <- filter(data, !comment %in% 'recount')
 
 #first look at the data
 ggplot(data, aes(x = sampling, y = cells_ml, fill = species))+
@@ -33,18 +34,17 @@ ggplot(data, aes(x = sampling, y = cells_ml, fill = species))+
 treatments <- read.csv2('~/Desktop/MA/MA_Rcode/project_data/treatments_units.csv')
 str(treatments)
 names(treatments) = c('MC', 'fluctuation', 'treatment')
-rich <- left_join(data1, treatments, by = c('MC'))  %>%
-  select(-comment)
+rich <- left_join(data, treatments, by = c('MC')) %>%
+  separate(species, into = c('genus', 'species'), ' ') 
 
 
 shannon <- rich %>%
-  group_by(treatment, sampling, species) %>%
+  select(-species)  %>%
+  group_by(treatment, sampling, genus) %>%
   summarise(mean_cells = mean(cells_ml, na.rm = T)) %>%
   ungroup()%>%
-  spread(key = species, value = mean_cells) %>%
-  group_by(treatment, sampling) %>%
-  mutate(Gymnodinium_sp = sum(Gymnodinium, Gymnodinium_Lepodinium)) %>%
-  select(-Gymnodinium_Lepodinium, -Gymnodinium)
+  spread(key = genus, value = mean_cells) %>%
+  group_by(treatment, sampling) 
  
 #remove NAs and exchange with 0
 shannon[is.na(shannon)] <- 0
@@ -64,4 +64,52 @@ ggplot(shannon, aes(x = sampling, y = shan, col = treatment)) +
   theme_bw()
 
 
-  
+#### Biovolume ####
+algal_measurement <- read_delim("~/Desktop/MA/MA_Rcode/project_data/algal_measurement.csv", 
+                                ";", escape_double = FALSE, locale = locale(decimal_mark = ","), 
+                                trim_ws = TRUE)
+str(algal_measurement)
+
+#calculate mean biovolume
+BioV <- algal_measurement %>%
+  group_by(genus, species, id) %>%
+  summarise(mean_BioV = mean(BioV, na.rm = T),
+            sd = sd(BioV, na.rm = T)) %>%
+  ungroup()
+
+## all_data
+all_data <- left_join(rich, BioV, by = c('genus', 'species') ) %>%
+ select(treatment, fluctuation, MC, sampling, genus, species, id, cells_ml,mean_BioV) %>%
+  mutate(volume = cells_ml*mean_BioV) 
+
+#new df to calculate mean biovolume
+calc_BV <- all_data%>%
+  filter(sampling != 0) %>%
+  group_by(treatment, fluctuation, sampling, genus, species, id) %>%
+  summarise(mean_V = mean(volume, na.rm = T)) %>% #calculate mean cells with biovolume
+  mutate(sp_id = paste(genus,species, sep = '_'))
+
+#calculate mean Volume for sampling 0, assuming all treatments are the same at the beginning
+data0 <- all_data %>% 
+  filter(sampling == 0) %>%
+  mutate(sp_id = paste(genus,species, sep = '_')) %>%
+  group_by(genus, species, sp_id, sampling, id) %>%
+  summarise(con = mean(volume, na.rm = T),
+            F6 = mean(volume, na.rm = T),
+            F12 = mean(volume, na.rm = T),
+            F24 = mean(volume, na.rm = T),
+            F36 = mean(volume, na.rm = T),
+            F48 = mean(volume, na.rm = T)) %>%
+  gather(key = 'treatment', value = 'mean_V', -genus, -species,-sp_id, -sampling, -id)
+
+##join zero-values and rest
+all_BV <- dplyr::bind_rows(calc_BV, data0) %>%
+  drop_na(mean_V) %>%
+  group_by(id, sampling, treatment) %>%
+  summarise(sum = sum(mean_V))
+
+ggplot(all_BV, aes(x = sampling, y = sum, col = treatment))+
+  geom_jitter(width = 0.25,size = 2)+
+  scale_x_continuous(limits = c(-1, 19), breaks = c(0,6,10,14,18))+
+  facet_wrap(~id, scales = 'free_y')+
+  theme_bw()
